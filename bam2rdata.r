9d9d22c07ca6a2d873d66e697f30fde3065e6b11
Tatsunori Hashimoto <thashim@pax6.csail.mit.edu>
2014-03-16 00:45:33 -0400

    Filter on map quality and clean common.r

--- bam2rdata.r
+++ bam2rdata.r
@@ -16,17 +16,19 @@ source(commondir)
 ###
 # read bam
 
-plusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=F,isDuplicate=F,isNotPassingQualityControls=F))
+plusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=F,isDuplicate=F,isNotPassingQualityControls=F),what=c('mapq'))
 plusstrand = readBamGappedAlignments(bamname,param=plusflags)
 
-minusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=T,isDuplicate=F,isNotPassingQualityControls=F))
+minusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=T,isDuplicate=F,isNotPassingQualityControls=F),what=c('mapq'))
 minusstrand = readBamGappedAlignments(bamname,param=minusflags)
 
-obschrnames=levels(c(seqnames(plusstrand),seqnames(minusstrand)))	
+obschrnames=levels(c(seqnames(plusstrand),seqnames(minusstrand)))
 allreads=lapply(obschrnames,function(chr){
-	print(chr)	
-	pluscoord=start(plusstrand[seqnames(plusstrand)==chr])
-        minuscoord=start(minusstrand[seqnames(minusstrand)==chr])
+	print(chr)
+        select = (seqnames(plusstrand)==chr) & (mcols(plusstrand)$mapq > mapq)
+	pluscoord=start(plusstrand[select])
+        select = (seqnames(minusstrand)==chr) & (mcols(minusstrand)$mapq > mapq)
+        minuscoord=start(minusstrand[select])
 	list(plus=pluscoord,minus=minuscoord)
 })
 names(allreads)=obschrnames
---
+++ common.global.r
@@ -0,0 +1,90 @@
+#####
+# utils
+
+old.repos <- getOption("repos")
+on.exit(options(repos = old.repos))
+new.repos <- old.repos
+new.repos["CRAN"] <- "http://cran.stat.ucla.edu"
+options(repos = new.repos)
+
+#source("http://www.bioconductor.org/biocLite.R")
+if(!suppressMessages(require("BiocInstaller",quietly=T))){
+    install.packages("BiocInstaller", repos="http://www.bioconductor.org/packages/2.13/bioc")
+}
+
+ris <- function(x){if(!require(x,character.only=T,quietly=T,warn.conflicts=F)){
+    install.packages(x)
+    require(x,character.only=T,quietly=T,warn.conflicts=F)
+}}
+bis <- function(x){if(!require(x,character.only=T,quietly=T,warn.conflicts=F)){
+    biocLite(x)
+    require(x,character.only=T,quietly=T,warn.conflicts=F)
+}}
+
+wipetemp <- function(){
+    x <- readline("this will wipe existing temp files (y/n)")
+    if(x=="y"){
+        print("wiping temp")
+        unlink("tmp/*")
+    }else{
+        print("keeping temp")
+    }
+}
+
+set.seed(1)
+
+####
+# dependencies
+
+suppressMessages(require('BiocInstaller',quietly=T))
+suppressMessages(bis("BSgenome.Hsapiens.UCSC.hg19"))
+suppressMessages(bis("Rsamtools"))
+suppressMessages(bis('Biostrings'))
+suppressMessages(bis('seqLogo'))
+
+suppressMessages(ris('RSofia'))
+suppressMessages(ris('statmod'))
+suppressMessages(ris('Rcpp'))
+suppressMessages(ris('inline'))
+suppressMessages(require(Matrix,quietly=T))
+
+####
+# params
+
+#minimum map quality
+mapq = 1
+
+#window size
+wsize = 1000
+#fast mode
+fast.mode = T
+
+#if a file indicating completion is found; exit.
+overwrite=F
+
+
+#number of kmer samples to draw
+nkmer = 5000000
+#motif score cutoff (log)
+motifcut = 5
+#motif informativeness cutoff (nats)
+basecut = 0
+#max candidate sites (default 500k)
+maxcand = 500000
+#match reverse complement (setting to T matches ONLY the R.C.)
+match.rc = T
+
+
+#window size
+wsize = 1000
+
+#fast mode
+fast.mode = T
+
+#purity of calls
+purity.cut = 0.7
+
+# load genome file
+
+genome = Hsapiens
+
