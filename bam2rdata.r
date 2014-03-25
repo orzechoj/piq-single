4f33aaaf9c3e0674862efd60546756f449406292
Tatsunori Hashimoto <thashim@pax6.csail.mit.edu>
2014-03-25 11:59:24 -0400

    Handle replicates, rename variables

--- bam2rdata.r
+++ bam2rdata.r
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript
-#Rscript script commondir bamout
+#Rscript script commonfile bamout
 #example:
 #Rscript bam2rdata.r /cluster/thashim/basepiq/common.r /cluster/thashim/basepiq/tmp/bams.RData /cluster/cwo/dnase_seq/bams/D0_50-100_130801.bwa.mapq20.mm10.bam
 
@@ -7,14 +7,14 @@ options(echo=TRUE)
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
-commondir = args[1]
+commonfile = args[1]
 bamout = args[2]
+
+source(commonfile)
+
+#if only one bam..
+if(length(args)==3){
 bamname = args[3]
-
-source(commondir)
-
-###
-# read bam
 
 plusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=F,isDuplicate=F,isNotPassingQualityControls=F),what=c('mapq'))
 plusstrand = readBamGappedAlignments(bamname,param=plusflags)
@@ -35,3 +35,38 @@ names(allreads)=obschrnames
 
 save(allreads,file=bamout)
 
+}{
+#else multiple replicates to merge..
+    bamnames = args[3:length(args)]
+
+    bamlist=lapply(bamnames,function(bamname){
+        print(bamname)
+        plusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=F,isDuplicate=F,isNotPassingQualityControls=F),what=c('mapq'))
+        plusstrand = readBamGappedAlignments(bamname,param=plusflags)
+        #
+        minusflags = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=F,isMinusStrand=T,isDuplicate=F,isNotPassingQualityControls=F),what=c('mapq'))
+        minusstrand = readBamGappedAlignments(bamname,param=minusflags)
+        list(plusstrand,minusstrand)
+    })
+
+    obschrnames=unique(do.call(c,lapply(bamlist,function(i){
+        levels(c(seqnames(i[[1]]),seqnames(i[[2]])))
+    })))
+
+    allreads=lapply(obschrnames,function(chr){
+	print(chr)
+        pluscoord=do.call(c,lapply(bamlist,function(bam){
+            select = (seqnames(bam[[1]])==chr) & (mcols(bam[[1]])$mapq > mapq)
+            start(bam[[1]][select])
+        }))
+        minuscoord=do.call(c,lapply(bamlist,function(bam){
+            select = (seqnames(bam[[2]])==chr) & (mcols(bam[[2]])$mapq > mapq)
+            start(bam[[2]][select])
+        }))
+	list(plus=pluscoord,minus=minuscoord)
+    })
+    names(allreads)=obschrnames
+
+    save(allreads,file=bamout)
+
+}
