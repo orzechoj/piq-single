a6131c09eee0f90d704f9fe822572d39ba3a4ee3
Tatsunori Hashimoto <thashim@csail.mit.edu>
2014-07-09 20:42:29 +0000

    use biocLite not biocInstaller

--- common.global.r
+++ common.global.r
@@ -7,10 +7,10 @@ new.repos <- old.repos
 new.repos["CRAN"] <- "http://cran.stat.ucla.edu"
 options(repos = new.repos)
 
-#source("http://www.bioconductor.org/biocLite.R")
-if(!suppressMessages(require("BiocInstaller",quietly=T))){
-    install.packages("BiocInstaller", repos="http://www.bioconductor.org/packages/2.13/bioc")
-}
+source("http://www.bioconductor.org/biocLite.R")
+#if(!suppressMessages(require("BiocInstaller",quietly=T))){
+#    install.packages("BiocInstaller", repos="http://www.bioconductor.org/packages/2.13/bioc")
+#}
 
 ris <- function(x){if(!require(x,character.only=T,quietly=T,warn.conflicts=F)){
     install.packages(x)
